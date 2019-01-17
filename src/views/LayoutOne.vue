6e8b1fc02bf61769bb2bd09005604f8e6d060c74
Francis <fguttridge@gmail.com>
2019-01-17 13:05:27 -0500

    updated layoutOne to support changed view

--- src/views/LayoutOne.vue
+++ src/views/LayoutOne.vue
@@ -6,17 +6,40 @@
                 <source src="https://storage.googleapis.com/coverr-main/mp4/Mt_Baker.mp4" type="video/mp4">
               </video>
             </div>
-            <div class="container h-100">
+            <div class="video-container h-100">
                 <div class="d-flex h-100 text-center align-items-center">
                     <div class="text-white">
-                        <h1 class="card-text">Video Header</h1>
-                        <h2>With HTML5 Video and Bootstrap 4</h2>
+                        <h1 class="card-text">Typical Video Part Header</h1>
+                        <h2>For all your Cool Viewing Needs</h2>
                     </div>
                 </div>
             </div>
         </b-card>
         <div class="content-body">
-          <b-card-group deck class="mb-2">
+            <b-container fluid>
+                <b-row class="row-showcase">
+                    <b-col cols="5" class="col-align-left">
+                        <img src="../assets/target.svg">
+                    </b-col>
+                    <b-col cols="7" class="col-align-right">
+                        <div class="pretty-print">
+                            <h2>"Remember, this website provides several layouts, each of which is easily converted into a full scale site"</h2>
+                        </div>
+                    </b-col>
+                </b-row>
+                <b-row class="row-showcase">
+                    <b-col cols="7" class="col-align-left">
+                        <div class="pretty-print">
+                            <h2>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ullamcorper ipsum ut suscipit rutrum. Donec eu aliquet odio. Phasellus commodo euismod elit, eget efficitur est pretium ut. Nunc porttitor ligula vitae urna viverra, nec sollicitudin dolor porttitor. Mauris pellentesque felis vel imperdiet tincidunt. Nunc et pretium tortor. Aenean in placerat justo. Quisque congue turpis ac tellus molestie vestibulum quis id sapien. Nam pharetra, erat in euismod ultrices, urna nibh elementum velit, sed dictum diam lacus nec nulla. Nunc aliquet augue vestibulum enim viverra lacinia."</h2>
+                        </div>
+                    </b-col>
+                    <b-col cols="5" class="col-align-right">
+                        <img src="../assets/research.svg">
+                    </b-col>
+                </b-row>
+
+            </b-container>
+          <!-- <b-card-group deck class="mb-2">
           <b-card class="text-center" header="Card">
               some text
           </b-card>
@@ -31,7 +54,7 @@
             title="This is an Image Card"
             class="image-card mb-4">
             </b-card>
-          </b-card-group>
+          </b-card-group> -->
         </div>
     </div>
 </template>
@@ -42,7 +65,8 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Dancing+Script');
 #layout-one {
   text-align: center;
   overflow: auto;
@@ -53,10 +77,12 @@ export default {
   background-color: map-get($layoutOne, backgroundColor);
   padding: 0;
   color: map-get($layoutOne, textBodyColor);
+  max-height:100%;
+  height: 50rem;
 }
 .video-back {
   height: 50vh;
-  min-height: 25rem;
+  min-height: 35rem;
   overflow: hidden;
   width: 100%;
   opacity: 0.9;
@@ -76,7 +102,7 @@ export default {
   -webkit-transform: translateY(-25%);
   transform: translateY(-25%);
 }
-.container {
+.video-container {
   position: absolute;
   text-align: center;
   align-content: center;
@@ -105,9 +131,6 @@ export default {
     color: map-get($layoutOne, backgroundColor);
   }
 }
-.content-body {
-  padding: 1em 1em 1em 1em;
-}
 .image-card {
   color: map-get($layoutOne, backgroundColor) !important;
   width: 50%;
@@ -116,4 +139,55 @@ export default {
     opacity: 0.9;
   }
 }
+.row-showcase {
+    border-bottom: 5px double;
+    padding-top: 0.5rem;
+}
+.pretty-print {
+    display: flex;
+    font-family: 'Dancing Script', cursive;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    text-justify: center;
+    width: 60rem;
+    height: 100%;
+}
+.col-align-left {
+    align-items: left;
+    img {
+      height: 30rem;
+      width: 30rem;
+      float: left;
+      padding-bottom: 0.5rem;
+    }
+}
+.col-align-right {
+    align-items: right;
+    img {
+      height: 30rem;
+      width: 30rem;
+      float: right;
+      padding-bottom: 0.5rem;
+    }
+}
+  /* width */
+    ::-webkit-scrollbar {
+    width: 5px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+    background: map-get($theme, primary)
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+    background: map-get($theme, highlight); 
+    }
 </style>
