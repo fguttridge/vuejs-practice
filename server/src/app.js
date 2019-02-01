fde26f85423b16d168b75b108df4c44ac0e95937
Francis <fguttridge@gmail.com>
2019-02-01 09:57:29 -0500

    added argon2i password for password security

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const argon = require('argon2');
 
 const app = express();
 var User = require('../model/user');
@@ -12,7 +13,7 @@ app.use(cors());
 app.listen(process.env.PORT || 8081);
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://fguttridge:AlwaysR3m3mb3r@ds042128.mlab.com:42128/fgutt_testweb', { useNewUrlParser: true });
+mongoose.connect('mongodb://fguttridge:AlwaysR3m3mb3r@ds058739.mlab.com:58739/fgutt_testweb', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function(callback){
@@ -28,22 +29,61 @@ app.get('/posts', (req, res) => {
     )
   });
 
-app.post('/createUser', (req,res) => {
+  // User management endpoints
+app.post('/createUser', async (req,res) => {
     var db = req.db;
-    var new_user = new User({
-      principle: req.body.principle,
-      publicKey: req.body.pubKey,
-      privateKey: req.body.privKey,
-      email: req.body.email,
-      passwordHash: req.body.password
+    // Use argon2 to create secure password
+    try {
+      let hash = await argon.hash(req.body.password).then(hash => {
+      // Send password to mongodb
+      var new_user = new User({
+        principle: req.body.principle,
+        publicKey: req.body.pubKey,
+        privateKey: req.body.privKey,
+        email: req.body.email,
+        passwordHash: hash
+      });
+      new_user.save(function (error) {
+        if (error) {
+          console.log(error)
+        }
+        res.send({
+          success: true,
+          message: 'User saved successfully!'
+        })
+      });
     });
-    new_user.save(function (error) {
-      if (error) {
-        console.log(error)
-      }
-      res.send({
-        success: true,
-        message: 'User saved successfully!'
-      })
-    })
+    } catch (err) {
+      console.log(err);
+    };
+
 });
+
+app.post('/login', async (req, res) => {
+  let foundUser
+  await User.find({"principle": req.body.principle}, function (error, user) {
+    foundUser = user
+    if (foundUser === undefined) {
+      res.send({
+        errorCode: "001",
+        message: "The username or password did not match"
+      });
+    }
+  })
+  .sort({_id:-1})
+  .catch(err => console.log(err));
+  try {
+    if (await argon.verify(foundUser[0].passwordHash, req.body.password)) {
+      res.send({
+        user: foundUser[0]
+      });
+    } else {
+      res.send({
+        errorCode: "001",
+        message: "The username or password did not match"
+      });
+    }
+  } catch(err) {
+    console.log(err);
+  }
+})
